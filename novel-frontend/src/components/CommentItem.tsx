9d115d517f89c7f77dbf4191fb64b9ba5cff6713
jackie1950057 <2821166215@qq.com>
2024-01-17 15:15:13 +0800

    update

--- novel-frontend/src/components/CommentItem.tsx
+++ novel-frontend/src/components/CommentItem.tsx
@@ -1,9 +1,13 @@
-
+import CloseIcon from '@mui/icons-material/Close';
 export default function Comment({ commentData }:{commentData:object}) {
-    return <div class="px-2.5 py-1.5 w-full">
-        <div class="text-gray-500 overflow-wrap break-words text-sm leading-tight pr-2">
+    return <div className="px-2.5 py-1.5 w-full">
+        <div className="text-gray-500 overflow-wrap break-words text-sm leading-tight pr-2">
             {commentData.address}
         </div>
-        <div class="text-md mt-1 text-gray-700">{commentData.content}</div>
+        <div className="text-md mt-1 text-gray-700">{commentData.content}</div>
+        <div className="flex justify-between">
+            <div className="text-xs mt-0.5  text-gray-400">{commentData.time}</div>
+            <CloseIcon className="scale-75 hidden text-gray-400"></CloseIcon>
+        </div>
     </div>
 }
