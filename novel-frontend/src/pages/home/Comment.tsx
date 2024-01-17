6063a8159b92f40586600157c7c40b26ebe5aff6
jackie1950057 <2821166215@qq.com>
2024-01-17 14:03:20 +0800

    update

--- novel-frontend/src/pages/home/Comment.tsx
+++ novel-frontend/src/pages/home/Comment.tsx
@@ -3,11 +3,32 @@
 export default function Comment({ isVisible }:{isVisible:boolean}) {
 
 
+
   return (
     <>
     {isVisible ? 
-    <div className="flex">
-      1234
+    <div className="flex flex-col bg-blue-100 w-1/5">
+      <div className="text-lg p-5 justify-between">
+        <div>
+          Comments
+        </div>
+        <div>
+          fsad
+        </div>
+      </div>
+      <div className="flex-1 min--0 overflow-auto">
+        <div style={
+          {
+            height: '1000px'
+          }
+        } className="">
+
+        </div>
+      </div>
+      <div className="bg-blue-50 p-5">
+        <input className="w-full outline-none bg-gray-200 p-1 rounded-md" />
+      </div>
+
     </div> : null}
     </>
     
