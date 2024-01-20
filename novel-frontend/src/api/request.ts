6a8f4879dcf85291a704cb820bffd4494906cf54
jackie1950057 <2821166215@qq.com>
2024-01-20 14:03:03 +0800

    udpate

--- novel-frontend/src/api/request.ts
+++ novel-frontend/src/api/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: 'http://localhost:5173/api',
+  baseURL: import.meta.env.VITE_APP_URL,
   timeout: 180000, // 请求超时时间毫秒
   withCredentials: true, // 异步请求携带cookie
   headers: {
--- novel-frontend/vite.config.ts
+++ novel-frontend/vite.config.ts
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   // 使用 loadEnv 获取环境变量
-  const env = loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_APP_URL, // 使用加载的环境变量
+          target: 'http://149.129.176.52:8080', // 使用加载的环境变量
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         },
