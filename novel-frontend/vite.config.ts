43bca79176152b923f16e77e5f45ce5dfcafd9e3
jackie1950057 <2821166215@qq.com>
2024-01-20 13:59:04 +0800

    update

--- novel-frontend/vite.config.ts
+++ novel-frontend/vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // 代理规则：将所有 /api 开头的请求代理到 http://localhost:3000
-      '/api': {
-        target: 'http://149.129.176.52:8080', // 后端 API 服务器地址
-        changeOrigin: true, // 需要虚拟主机站点
-        rewrite: (path) => path.replace(/^\/api/, '') // 重写请求路径，去除 /api
-      },
-      // 你可以添加更多的代理规则
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  // 使用 loadEnv 获取环境变量
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_URL, // 使用加载的环境变量
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        // 更多代理规则...
+      }
     }
-  }
-})
+  });
+};
+
