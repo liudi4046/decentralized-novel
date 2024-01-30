a43ef524a647f7ebb211dfc5219b660d3272bae4
Hpc's Windows <fhldhpc@outlook.com>
2024-01-31 07:04:17 +0800

    Modified deploy and test config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,45 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
-
 dotenv.config();
+const { ALCHEMY_SEPOLIA_KEY, ACCOUNT_PRIVATE_KEY, ACCOUNT_PRIVATE_KEY_2 } = process.env;
+
+// 使用方法：在添加.env文件，在其中加入 ALCHEMY_SEPOLIA_KEY, ACCOUNT_PRIVATE_KEY, ACCOUNT_PRIVATE_KEY_2后
+// 运行 npx hardhat test --network hardhat 进行本地部署测试
+// 运行 npx hardhat test --network sepolia 即可完成sepolia部署+测试
+
+
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`, // 使用Alchemy的Goerli节点
-      accounts: [process.env.PRIVATE_KEY as string],
-      chainId: 5,
+    // hardhat: {},
+    // Sepolia网络配置，使用Alchemy
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`,// 使用Alchemy的Sepolia节点
+      accounts: [ACCOUNT_PRIVATE_KEY!,ACCOUNT_PRIVATE_KEY_2!]
+      // chainId: 11155111,                    // Sepolia网络的链ID，非必须，Infura、Alchemy 都能够自动处理chainId
     },
+
+    // goerli: {
+      // 这个url应该废弃掉了，所以刘神才部署错误
+    //   url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`, // 使用Alchemy的Goerli节点
+    //   accounts: [process.env.PRIVATE_KEY as string],
+    //   chainId: 5,
+    // },
   },
   typechain: {
     outDir: "novel-frontend/typechain",
     target: "ethers-v6",
   },
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_KEY,
+    }
+  }
 };
 
 export default config;
--- test/test.ts
+++ test/test.ts
@@ -7,10 +7,17 @@ import {
   NovelManagement,
   NovelManagement__factory,
 } from "../typechain";
-import { DecentralizedNovelVoteToken } from "../typechain-types";
+
+// import { DecentralizedNovelVoteToken } from "../typechain-types";
+import { DecentralizedNovelVoteToken } from "../typechain";
+
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("test", () => {
+
+  let ini_supply = 1000;
+  let signer001Token: any;
+  let signer002Token: any;
   let novelManagementAddress: string;
   let VoteToken: DecentralizedNovelVoteToken__factory,
     voteToken: DecentralizedNovelVoteToken;
@@ -19,32 +26,60 @@ describe("test", () => {
   let NFTContract: DecentralizedNovelChapter__factory,
     nftContract: DecentralizedNovelChapter;
   let signers: HardhatEthersSigner[];
-  beforeEach(async () => {
+
+  before(async () => {
     //每个it开始前，部署三个合约，并且得到signers
     NovelManagement = await ethers.getContractFactory("NovelManagement");
     novelManagement = await NovelManagement.deploy();
+    // 在这里输出NovelManagement合约的地址
+    novelManagementAddress = await novelManagement.getAddress();
+    console.log("NovelManagement Address:", novelManagementAddress);
+
 
     VoteToken = await ethers.getContractFactory("DecentralizedNovelVoteToken");
     voteToken = await VoteToken.deploy(
-      1000,
+      ini_supply, // 开始的供应量
       await novelManagement.getAddress()
     );
+    console.log("voteToken Address:", await voteToken.getAddress());
+
     NFTContract = await ethers.getContractFactory("DecentralizedNovelChapter");
-    nftContract = await NFTContract.deploy(await novelManagement.getAddress());
+    nftContract = await NFTContract.deploy(
+      await novelManagement.getAddress()
+    );
+    console.log("nftContract Address:", await nftContract.getAddress());
+
     signers = await ethers.getSigners();
   });
+
+
+
+
+
+  
+
   it("deployment vote contract should reward token to deployer", async () => {
     const signerAddress = signers[0].address;
-    expect(await voteToken.balanceOf(signerAddress)).to.equal(1000);
+    console.log("signer001Address:", signerAddress);
+    signer001Token = await voteToken.balanceOf(signerAddress)
+    console.log("signer001Token:", signer001Token);
+    expect(signer001Token).to.equal(ini_supply);
   });
+
+
+
   it("anyone can submit a new chapter", async () => {
     await novelManagement
       .connect(signers[1])
       .submit("this is a content submitted by signers[1]");
+    console.log("signer002Address:", signers[1].address);
     expect((await novelManagement.submissions(0)).content).to.equal(
       "this is a content submitted by signers[1]"
     );
   });
+
+
+
   it("once a user has token, he should be able to vote", async () => {
     //set nft contract address and token contract address in novelmanagement
     await novelManagement.setNFTAddress(await nftContract.getAddress());
@@ -55,8 +90,17 @@ describe("test", () => {
       .connect(signers[1])
       .submit("this is a content submitted by signers[1]");
 
+    signer001Token = await voteToken.balanceOf(signers[0].address)
+    console.log("signer001Token:", signer001Token);
+    signer002Token = await voteToken.balanceOf(signers[1].address)
+    console.log("signer002Token:", signer002Token);
+
     //signers[0] can vote cause he has token
+    // User (signers[0]) approves the NovelManagement contract to spend their tokens
+    await voteToken.connect(signers[0]).approve(await novelManagement.getAddress(), 50);
+    // Now signers[0] can vote, as the NovelManagement contract has been given allowance to spend tokens on their behalf
     await novelManagement.connect(signers[0]).vote(0);
+
     expect((await novelManagement.submissions(0)).yesVotes).to.equal(1);
     expect(await novelManagement.hasVoted(0, signers[0].address)).to.equal(
       true
